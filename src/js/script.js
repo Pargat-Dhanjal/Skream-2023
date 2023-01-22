29b122467e61bc50b1db3debdc32f4a498b9411f
Killjoy <aryaajitnair@gmail.com>
2023-01-22 12:27:53 +0530

    feat: Add swiper and images on Landing Page

--- src/js/script.js
+++ src/js/script.js
@@ -5,73 +5,77 @@ canvas.height = window.innerHeight;
 const context = canvas.getContext("2d");
 const frameCount = 179;
 
-const currentFrame = (index) =>
-  `./src/img/balls/${(index + 1).toString()}.jpg`;
+const currentFrame = (index) => `./src/img/balls/${(index + 1).toString()}.jpg`;
 
 const images = [];
 let ball = { frame: 0 };
 
 for (let i = 0; i < frameCount; i++) {
-  const img = new Image();
-  img.src = currentFrame(i);
-  images.push(img);
+	const img = new Image();
+	img.src = currentFrame(i);
+	images.push(img);
 }
 
 gsap.to(ball, {
-  frame: frameCount - 1,
-  snap: "frame",
-  ease: "none",
-  scrollTrigger: {
-    scrub: 0.5,
-    pin: "canvas",
-    end: "500%",
-  },
-  onUpdate: render,
+	frame: frameCount - 1,
+	snap: "frame",
+	ease: "none",
+	scrollTrigger: {
+		scrub: 0.5,
+		pin: "canvas",
+		end: "500%",
+	},
+	onUpdate: render,
 });
 
 gsap.fromTo(
-  ".kjsce",
-  {
-    opacity: 0,
-  },
-  {
-    opacity: 1,
-    scrollTrigger: {
-      scrub: 1,
-      start: "10%",
-      end: "40%",
-    },
-    onComplete: () => {
-      gsap.to(".kjsce", { opacity: 0 });
-    },
-  }
+	".kjsce",
+	{
+		opacity: 0,
+	},
+	{
+		opacity: 1,
+		scrollTrigger: {
+			scrub: 1,
+			start: "10%",
+			end: "40%",
+		},
+		onComplete: () => {
+			gsap.to(".kjsce", { opacity: 0 });
+		},
+	}
 );
 
 gsap.fromTo(
-  ".skream",
-  {
-    opacity: 0,
-  },
-  {
-    opacity: 1,
-    scrollTrigger: {
-      scrub: 1,
-      start: "70%",
-      end: "90%",
-    },
-    onComplete: () => {
-      gsap.to(".skream", { opacity: 0 });
-    },
-  }
+	".skream",
+	{
+		opacity: 0,
+	},
+	{
+		opacity: 1,
+		scrollTrigger: {
+			scrub: 1,
+			start: "70%",
+			end: "90%",
+		},
+		onComplete: () => {
+			gsap.to(".skream", { opacity: 0 });
+		},
+	}
 );
-
 
 images[0].onload = render;
 
 function render() {
-  context.canvas.width = images[0].width;
-  context.canvas.height = images[0].height;
+	context.canvas.width = images[0].width;
+	context.canvas.height = images[0].height;
 
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  context.drawImage(images[ball.frame], 0, 0);
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	context.drawImage(images[ball.frame], 0, 0);
 }
+
+var swiper = new Swiper(".mySwiper", {
+	autoplay: {
+		delay: 2000,
+	},
+});
