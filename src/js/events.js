a3d0f4d259424c5f8ee497dba5a15f488c9a1dec
Vinay <vinaykanse9451@gmail.com>
2023-01-30 20:37:48 +0530

    fix: Modal overlay issues, changed css for modal

--- src/js/events.js
+++ src/js/events.js
@@ -1,4 +1,11 @@
-function handleClick(name, imagesrc) {
-	console.log(name,imagesrc);
-	document.getElementById("overlay").style.display = "block";
-}
+const handleClick = (name, imageSrc) => {
+  console.log(name, imageSrc);
+  document.getElementById("modal-image").setAttribute("src", imageSrc);
+  document.getElementById("overlay").style.display = "flex";
+  document.body.style.position = "fixed";
+};
+
+const closeModal = () => {
+  document.body.style.position = "initial";
+  document.getElementById("overlay").style.display = "none";
+};
